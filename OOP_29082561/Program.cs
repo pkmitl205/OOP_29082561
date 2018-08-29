439551b7a00e25d6101060de8801ae96dba9efda
pkmitl205 <58030205@kmitl.ac.th>
2018-08-29 11:08:41 +0700

    Edited - 1

--- OOP_29082561/Program.cs
+++ OOP_29082561/Program.cs
@@ -10,6 +10,36 @@ namespace OOP_29082561
     {
         static void Main(string[] args)
         {
+            //  2. instantiation of student
+            student stu1 = new student();
+            stu1.gender = "male";
+            stu1.SetName("Pakon");
+            stu1.SetGPA(3.5f);
+            stu1.setID("58030205");
+        }
+    }
+
+    // 1.Create class student
+    class student
+    {
+        private string id;
+        private string name;
+        public string gender;
+        private float gpa;
+
+        public void SetName(string name)
+        {
+            this.name = name;
+        }
+
+        internal void SetGPA(float v)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void setID(string v)
+        {
+            throw new NotImplementedException();
         }
     }
 }
